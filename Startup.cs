61738298cd0ebaa444ca56b7c0ebf07be6e90438
auliahanifan <auliarahman80@gmail.com>
2020-08-04 12:27:23 +0700

    berhasil pakai sqlite

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace LatihanApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<SqliteDbContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<SqliteDbContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<IUserService, UserService>();
